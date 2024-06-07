d0ff9cf4e16e70b56ab2ef6766909946c78fea3b
Mevek <148761491+Mevek@users.noreply.github.com>
2024-06-07 10:20:23 +0200

    v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <p className=" text-green-600 text-5xl font-bold">Mikołaj Łysio</p>
-      <p className="text-xl">Nr albumu: 87203</p>
-      
+      <p className=" text-blue-400 text-5xl font-bold">Mikołaj Łysio</p>
+      <p className="text-green-300 text-3xl font-bold" >Nr albumu: 87203</p>
+
+      <Link href="/strona">Przejdź na drugą stronę</Link>
+      <div className="mt-2">
+        <Button className="bg-white hover:bg-red-700">Kliknij</Button>
+      </div>
+
     </div>
   );
 }
---
+++ app/strona/page.tsx
@@ -0,0 +1,8 @@
+export default function Strona() {
+    return (
+        <div className="h-screen flex flex-col items-center justify-center">
+          <p className=" text-blue-400 text-5xl font-bold">Mikołaj Łysio</p>
+          <p className="text-green-300 text-3xl font-bold" >Nr albumu: 87203</p>
+        </div>
+    );
+}
